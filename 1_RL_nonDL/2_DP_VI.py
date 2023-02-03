ae6ab35aa40b3e1af1254ed5fe0fd46c109e2cdc
Seil Jun <ypd0709@naver.com>
2023-02-04 03:10:11 +0900

    2nd commit
    rearrange & DP_PI DP_VI

---
+++ 1_RL_nonDL/2_DP_VI.py
@@ -0,0 +1,19 @@
+# FrozenLake-v1 / MDP value iteration
+import gym
+from gym.envs.toy_text.frozen_lake import generate_random_map
+from src.mdp import mdp
+
+env = gym.make('FrozenLake-v1', desc=generate_random_map(size=8), is_slippery=False, render_mode='human')
+env.reset()
+
+agent = mdp()
+agent.initialize_env(env)
+policy = agent.value_itration()
+
+s = 0
+while True:
+    env.render()
+    ns, reward, done, tmp, info = env.step(policy[s].argmax(axis=0))
+    s = ns
+    if done:
+        break
--- study/Fastcampus/1_DP_PI.py
+++
@@ -1,3 +0,0 @@
-# known model
-# policy iteration
-
