751def7e71de027adbdce49d71a100e59d6a6f53
Seil Jun <ypd0709@naver.com>
2023-08-16 14:47:20 +0900

    23.08.16

--- 2_Advanced_RL/3_REINFORCE/1_breakout_reinforce.py
+++ 2_Advanced_RL/3_REINFORCE/1_breakout_reinforce.py
@@ -1,4 +1,17 @@
+import sys
+sys.path.append('C:/Users/ypd07/PycharmProjects/study/RL/2_Advanced_RL')
+for i in sys.path:
+    print(i)
+
 import gym
+import numpy as np
+import torch
+import torch.nn as nn
+import os
+from matplotlib import pyplot as plt
+from src.MLP import MLP
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 env = gym.make('Breakout-v4', render_mode='human')
 env.reset()
@@ -7,7 +20,7 @@ for _ in range(100):
     env.reset()
     cnt = 0
     while True:
-        ns, reward, done, tmp, info = env.step(env.action_space.sample())
+        ns, reward, done, truncation, info = env.step(env.action_space.sample())
         cnt += 1
         if done:
             break
