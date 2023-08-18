9f40886e7c441e33f5b74cd4ca6de6b1a4b97673
Seil Jun <ypd0709@naver.com>
2023-08-18 16:12:13 +0900

    23.08.18

--- 2_Advanced_RL/algorithm.py
+++ 2_Advanced_RL/algorithm.py
@@ -3,17 +3,8 @@ input = sys.stdin.readline
 
 n = int(input())
 
-def f(x):
-    res = 0
-    for i in range(1, int(x**(1/2))+1):
-        if x % i == 0:
-            res += i
-            if i != x// i:
-                res += x // i
-    return res
-
 s = 0
-for x in range(1, n+1):
-    s += f(x)
-    
+for i in range(1, n+1):
+    s += (n // i) * i
+
 print(s)
